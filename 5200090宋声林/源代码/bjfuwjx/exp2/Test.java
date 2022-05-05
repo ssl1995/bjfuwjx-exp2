9853d77bf66a8188f5701fad8a5c03e7b31151ff
songshenglin <872912679@qq.com>
2022-05-05 08:12:16 +0800

    init

---
+++ 5200090宋声林/源代码/bjfuwjx/exp2/Test.java
@@ -0,0 +1,12 @@
+package bjfuwjx.exp2;
+
+/**
+ * @author SongShengLin
+ * @date 2022/5/5 08:10
+ * @description
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
